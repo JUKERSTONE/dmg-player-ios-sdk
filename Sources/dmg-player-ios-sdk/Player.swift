9282b70daa80680317f100cb8d8f6f347ac8da05
TSB <tobishittabey@gmail.com>
2024-02-26 13:12:00 +0000

    [iDMG] : startup changes

--- Sources/dmg-player-ios-sdk/Player.swift
+++ Sources/dmg-player-ios-sdk/Player.swift
@@ -20,7 +20,40 @@ public class TrackPlayerSDK: NSObject {
 
 // Make sure to conform to WKNavigationDelegate if needed.
 extension TrackPlayerSDK: WKNavigationDelegate {
-    // Implement the delegate methods here...
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // Inject JavaScript here after page load
+        let jsCode = """
+        if (!window.trakStarVideo) {
+                window.trakStarVideo = document.getElementsByTagName('video')[0];
+              }
+              
+              if (window.trakStarVideo) {
+                window.trakStarVideo.requestPictureInPicture().then(() => {
+                  const message = {
+                    eventType: 'enablePiP',
+                    data: 'PiP initiated successfully.'
+                  };
+                  window.ReactNativeWebView.postMessage(JSON.stringify(message));
+                }).catch(error => {
+                  const message = {
+                    eventType: 'enablePiP',
+                    data: 'PiP initiation failed: ' + error.message
+                  };
+                  window.ReactNativeWebView.postMessage(JSON.stringify(message));
+                });
+              } else {
+                const message = {
+                  eventType: 'enablePiP',
+                  data: 'No video element found.'
+                };
+                window.ReactNativeWebView.postMessage(JSON.stringify(message));
+              }
+              true;
+        """
+        webView.evaluateJavaScript(jsCode, completionHandler: nil)
+    }
+    
+    // Implement other WKNavigationDelegate methods as needed
 }
 
 
