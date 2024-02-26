03058cc907b4874d23fa89bb7f8fd1daf5c6a950
TSB <tobishittabey@gmail.com>
2024-02-26 12:16:49 +0000

    [iDMG] : startup changes

--- Sources/dmg-player-ios-sdk/dmg_player_ios_sdk.swift
+++ Sources/dmg-player-ios-sdk/dmg_player_ios_sdk.swift
@@ -1,21 +1,35 @@
 import Foundation
+import SwiftUI // This import is necessary for SwiftUI-specific types
 import WebKit
 
 public class TrackPlayerSDK: NSObject, WKNavigationDelegate {
     public var webView: WKWebView?
+    public var currentTrack: Track?
     
     public override init() {
         super.init()
         let config = WKWebViewConfiguration()
-        // Configuration here, if needed...
-        self.webView = WKWebView(frame: .zero, configuration: config)
-        self.webView?.navigationDelegate = self
+        webView = WKWebView(frame: .zero, configuration: config)
+        webView?.navigationDelegate = self
     }
     
     public func playNow(track: Track) {
+        currentTrack = track
         guard let webView = webView else { return }
         webView.load(URLRequest(url: track.url))
     }
     
-    // Implement the WKNavigationDelegate methods here...
+    public func makeUIView() -> WKWebView {
+        return webView ?? WKWebView()
+    }
+    
+    public func updateUIView(_ uiView: WKWebView) {
+        if let track = currentTrack {
+            uiView.load(URLRequest(url: track.url))
+        }
+    }
+    
+    // WKNavigationDelegate methods...
 }
+
+
--- Sources/dmg-player-ios-sdk/trx.swift
+++ Sources/dmg-player-ios-sdk/trx.swift
@@ -2,7 +2,6 @@ import Foundation
 
 public struct Track {
     public let url: URL
-    
     public init(url: URL) {
         self.url = url
     }
