e0a27816f16067024f5c299a057abab0a758b3e0
TSB1999 <hakeems@quickcommerce.org>
2024-02-27 19:52:02 +0000

    [iDMG] : startup changes

--- Sources/dmg-player-ios-sdk/Player/DMGPlayer.swift
+++ Sources/dmg-player-ios-sdk/Player/DMGPlayer.swift
@@ -110,7 +110,7 @@ public class TrackPlayerSDK: NSObject, ObservableObject, WKScriptMessageHandler 
     }
     
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if message.name == "handlerName" {
+        if message.name == "player" {
             print("Received message from web: \(message.body)")
             if let messageBody = message.body as? [String: Any] {
                 if let eventType = messageBody["eventType"] as? String {
