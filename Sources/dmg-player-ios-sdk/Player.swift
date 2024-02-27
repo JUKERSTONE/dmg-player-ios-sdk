4ef99749a5311eb9ce7c1f69053adb23e546acfb
TSB <tobishittabey@gmail.com>
2024-02-27 11:41:42 +0000

    [iDMG] : startup changes

--- Sources/dmg-player-ios-sdk/Player.swift
+++ Sources/dmg-player-ios-sdk/Player.swift
@@ -27,6 +27,7 @@ public class TrackPlayerSDK: NSObject, ObservableObject, WKScriptMessageHandler 
         let config = WKWebViewConfiguration()
         let userContentController = WKUserContentController()
         userContentController.add(self, name: "videoEnded")
+        userContentController.add(self, name: "videoCurrentTime")
         config.userContentController = userContentController
 
         // Properly initialize the WebViews with the configuration
@@ -103,31 +104,122 @@ public class TrackPlayerSDK: NSObject, ObservableObject, WKScriptMessageHandler 
     }
     
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if message.name == "videoEnded", let messageBody = message.body as? String, messageBody == "ended" {
-            DispatchQueue.main.async {
-                self.switchActiveAndInactiveWebViews()
+        if message.name == "videoEnded" {
+            if let messageBody = message.body as? String, messageBody == "ended" {
+                DispatchQueue.main.async {
+                    self.switchActiveAndInactiveWebViews()
+                }
+            }
+        } else if message.name == "videoCurrentTime" {
+            if let messageBody = message.body as? [String: Any], let eventType = messageBody["eventType"] as? String {
+                if eventType == "videoCurrentTime" {
+                    // Handle video progress message
+                    if let progressData = messageBody["data"] as? Double {
+                        // Process the progress data
+                        if progressData > 80.0 {
+                            // Preload the inactive web view
+                            preloadInactiveWebView()
+                        }
+                    }
+                }
             }
         }
     }
     
+    private func preloadInactiveWebView() {
+        guard let nextIsrc = queue.first else {
+            print("Queue is empty")
+            return
+        }
+
+        let apiService = APIService.shared
+        let urlString = "https://europe-west1-trx-traklist.cloudfunctions.net/TRX_DEVELOPER/trx/music/\(nextIsrc)"
+        
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        apiService.fetchData(from: url) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    guard let urlStringWithQuotes = String(data: data, encoding: .utf8) else {
+                        print("The data received could not be converted to a string.")
+                        return
+                    }
+                    
+                    print(urlStringWithQuotes)
+                    // Remove quotation marks from the string
+                    let urlString = urlStringWithQuotes.trimmingCharacters(in: CharacterSet(charactersIn: "\""))
+                 
+                    // Validate if the cleaned string is a valid URL
+                    guard let videoURL = URL(string: urlString) else {
+                        print("The cleaned string is not a valid URL: \(urlString)")
+                        return
+                    }
+                    
+                    // Load the URL in the inactive WebView
+                    self?.loadVideoInInactiveWebView(url: videoURL)
+                    
+                case .failure(let error):
+                    print("Error fetching data: \(error)")
+                }
+            }
+        }
+    }
+
+    private func loadVideoInInactiveWebView(url: URL) {
+        let request = URLRequest(url: url)
+        inactiveWebView.load(request)
+        
+        // Execute JavaScript to mute and pause the video
+        let script = """
+            var video = document.querySelector('video');
+            if (video) {
+                video.muted = true;
+                video.pause();
+            }
+        """
+        inactiveWebView.evaluateJavaScript(script, completionHandler: nil)
+    }
+
+    
+    private func setupVideoProgressListener(webView: WKWebView) {
+        let script = """
+            window.trakStarVideo.addEventListener('timeupdate', () => {
+                window.webkit.messageHandlers.videoProgress.postMessage({
+                    eventType: 'videoProgress',
+                    data: (window.trakStarVideo.currentTime / window.trakStarVideo.duration) * 100
+                });
+            });
+        """
+        webView.evaluateJavaScript(script, completionHandler: nil)
+    }
+
+    
+    
     private func switchActiveAndInactiveWebViews() {
+        // Mute and pause the active player
+        muteAndPause(webView: activeWebView)
+        
         // Swap the references
         (activeWebView, inactiveWebView) = (inactiveWebView, activeWebView)
         
-        // Load next video in the new activeWebView if necessary
-        // You may also want to mute and pause the new inactiveWebView here
-        // This is just a placeholder call; implement based on your app's logic
-        muteAndPause(webView: inactiveWebView)
+        // Play the newly active player
+        playActiveWebView()
         
-//        if index < queue.count {
-//            let nextIsrc = queue[index]
-//            playNow(isrc: nextIsrc)
-//        } else {
-//            print("Index out of range")
-//        }
+        // Reset the index for the queue
+        index = (index + 1) % queue.count
         
-        // Load next video or perform other setup for the new active web view
     }
+
+    private func playActiveWebView() {
+        // Unmute and play the video in the new activeWebView
+        let script = "document.querySelector('video').muted = false; document.querySelector('video').play();"
+        activeWebView.evaluateJavaScript(script, completionHandler: nil)
+    }
+
     
     private func muteAndPause(webView: WKWebView) {
         let script = "document.querySelector('video').muted = true; document.querySelector('video').pause();"
@@ -135,10 +227,45 @@ public class TrackPlayerSDK: NSObject, ObservableObject, WKScriptMessageHandler 
     }
     
     private func preloadNextVideo(isrc: String) {
-            // Similar logic to playNow, but for the inactive webview
-            // This function should be called after a video is loaded in the active webview
-            // to prepare the next video in the inactive webview
+        let apiService = APIService.shared
+        let urlString = "https://europe-west1-trx-traklist.cloudfunctions.net/TRX_DEVELOPER/trx/music/\(isrc)"
+        
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        apiService.fetchData(from: url) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    guard let urlStringWithQuotes = String(data: data, encoding: .utf8) else {
+                        print("The data received could not be converted to a string.")
+                        return
+                    }
+                    
+                    // Remove quotation marks from the string
+                    let urlString = urlStringWithQuotes.trimmingCharacters(in: CharacterSet(charactersIn: "\""))
+                    
+                    // Validate if the cleaned string is a valid URL
+                    guard let videoURL = URL(string: urlString) else {
+                        print("The cleaned string is not a valid URL: \(urlString)")
+                        return
+                    }
+                    
+                    // Load the URL in the WebView
+                    self?.loadVideoInInactiveWebView(url: videoURL)
+                case .failure(let error):
+                    print("Error fetching data: \(error)")
+                }
+            }
+        }
     }
+        
+        private func loadVideoInInactiveWebView(url: URL) {
+            let request = URLRequest(url: url)
+            inactiveWebView.load(request)
+        }
 
     public func queueNext(isrc: String) {
         queue.insert(isrc, at: 0)
@@ -155,6 +282,7 @@ public class TrackPlayerSDK: NSObject, ObservableObject, WKScriptMessageHandler 
 //    public func removeFromQueue(track: Track) {
 //        webView.load(URLRequest(url: track.url))
 //    }
+    
 }
 
 // Make sure to conform to WKNavigationDelegate if needed.
--- Sources/dmg-player-ios-sdk/WebViewWrapper.swift
+++ Sources/dmg-player-ios-sdk/WebViewWrapper.swift
@@ -7,8 +7,8 @@ public struct WebViewWrapper: UIViewRepresentable {
     @ObservedObject var sdk: TrackPlayerSDK
     
     public init(sdk: TrackPlayerSDK) {
-        self.sdk = sdk
-    }
+            self.sdk = sdk
+        }
     
     public func makeUIView(context: Context) -> UIView {
         // Create a container view to hold both web views
@@ -40,42 +40,12 @@ public struct WebViewWrapper: UIViewRepresentable {
             inactiveWebView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
         ])
         
-        // Set up message handlers for both active and inactive web views
-        activeWebView.configuration.userContentController.add(context.coordinator, name: "activeWebViewEvent")
-        inactiveWebView.configuration.userContentController.add(context.coordinator, name: "inactiveWebViewEvent")
-        
+        // Return the container view
         return containerView
     }
     
     public func updateUIView(_ uiView: UIView, context: Context) {
         // Update the UI view if needed
         // This method will be called whenever SwiftUI thinks the view needs to be updated
-    }
-    
-    public func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-    
-    public class Coordinator: NSObject, WKScriptMessageHandler {
-        var parent: WebViewWrapper
-        
-        init(_ parent: WebViewWrapper) {
-            self.parent = parent
-        }
-        
-        public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-            // Handle messages received from both active and inactive web views
-            if message.name == "activeWebViewEvent" {
-                // Handle message received from the active web view
-                let body = message.body as? String ?? ""
-                print("Received message from active web view: \(body)")
-                // Process the message and take appropriate actions
-            } else if message.name == "inactiveWebViewEvent" {
-                // Handle message received from the inactive web view
-                let body = message.body as? String ?? ""
-                print("Received message from inactive web view: \(body)")
-                // Process the message and take appropriate actions
-            }
-        }
     }
 }
