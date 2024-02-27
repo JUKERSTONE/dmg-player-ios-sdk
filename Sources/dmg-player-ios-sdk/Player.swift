c2c799b49d9decf0ae128a5fa10779d2844861b5
TSB <tobishittabey@gmail.com>
2024-02-27 10:58:34 +0000

    [iDMG] : startup changes

--- Sources/dmg-player-ios-sdk/Player.swift
+++ Sources/dmg-player-ios-sdk/Player.swift
@@ -73,13 +73,13 @@ public class TrackPlayerSDK: NSObject, ObservableObject, WKScriptMessageHandler 
                         print(urlStringWithQuotes)
                         // Remove quotation marks from the string
                         let urlString = urlStringWithQuotes.trimmingCharacters(in: CharacterSet(charactersIn: "\""))
-                        print(urlString, "here")
+                     
                         // Validate if the cleaned string is a valid URL
                         guard let videoURL = URL(string: urlString) else {
                             print("The cleaned string is not a valid URL: \(urlString)")
                             return
                         }
-                        print(videoURL, "heere")
+                    
                         // Load the URL in the WebView
                         DispatchQueue.main.async { [weak self] in
                             self?.loadVideoInActiveWebView(url: videoURL)
@@ -189,62 +189,87 @@ public class TrackPlayerSDK: NSObject, ObservableObject, WKScriptMessageHandler 
 @available(iOS 13.0, *)
 extension TrackPlayerSDK: WKNavigationDelegate {
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        // Inject JavaScript here after page load
-        let jsCode = """
-        if (!window.trakStarVideo) {
+        // Inject JavaScript for both active and inactive web views
+        let jsCodeCommon = """
+            if (!window.trakStarVideo) {
                 window.trakStarVideo = document.getElementsByTagName('video')[0];
-              }
-              
-              if (window.trakStarVideo) {
+            }
+            
+            if (window.trakStarVideo) {
                 window.trakStarVideo.requestPictureInPicture().then(() => {
-                  const message = {
-                    eventType: 'enablePiP',
-                    data: 'PiP initiated successfully.'
-                  };
-                  window.ReactNativeWebView.postMessage(JSON.stringify(message));
+                    const message = {
+                        eventType: 'enablePiP',
+                        data: 'PiP initiated successfully.'
+                    };
+                    window.ReactNativeWebView.postMessage(JSON.stringify(message));
                 }).catch(error => {
-                  const message = {
-                    eventType: 'enablePiP',
-                    data: 'PiP initiation failed: ' + error.message
-                  };
-                  window.ReactNativeWebView.postMessage(JSON.stringify(message));
+                    const message = {
+                        eventType: 'enablePiP',
+                        data: 'PiP initiation failed: ' + error.message
+                    };
+                    window.ReactNativeWebView.postMessage(JSON.stringify(message));
                 });
-              } else {
+            } else {
                 const message = {
-                  eventType: 'enablePiP',
-                  data: 'No video element found.'
+                    eventType: 'enablePiP',
+                    data: 'No video element found.'
                 };
                 window.ReactNativeWebView.postMessage(JSON.stringify(message));
-              };
+            };
             window.trakStarVideo = document.getElementsByTagName('video')[0];
-                window.trakStarVideo.addEventListener('loadedmetadata', () => {
-                  window.ReactNativeWebView.postMessage(JSON.stringify({
-                      eventType: 'videoReady',
-                      data: true
-                  }));
-                });
-                window.trakStarVideo.addEventListener('ended', function() {
-                  window.ReactNativeWebView.postMessage(JSON.stringify({
+            
+            window.trakStarVideo.addEventListener('loadedmetadata', () => {
+                window.ReactNativeWebView.postMessage(JSON.stringify({
+                    eventType: 'videoReady',
+                    data: true
+                }));
+            });
+            
+            window.trakStarVideo.addEventListener('ended', function() {
+                window.ReactNativeWebView.postMessage(JSON.stringify({
                     eventType: 'videoEnded',
                     data: 100
-                  }));
-                });
-                window.trakStarVideo.addEventListener('timeupdate', () => {
-                  window.ReactNativeWebView.postMessage(JSON.stringify({
+                }));
+            });
+            
+            window.trakStarVideo.addEventListener('timeupdate', () => {
+                window.ReactNativeWebView.postMessage(JSON.stringify({
                     eventType: 'videoCurrentTime',
                     data: (window.trakStarVideo.currentTime / window.trakStarVideo.duration) * 100
-                  }));
-                });
-                window.trakStarVideo.addEventListener('error', function() {
-                  window.ReactNativeWebView.postMessage(JSON.stringify({
-                      eventType: 'videoError',
-                      data: 'An error occurred while trying to load the video.'
-                  }));
-                });
-              true;
+                }));
+            });
+            
+            window.trakStarVideo.addEventListener('error', function() {
+                window.ReactNativeWebView.postMessage(JSON.stringify({
+                    eventType: 'videoError',
+                    data: 'An error occurred while trying to load the video.'
+                }));
+            });
+            true;
         """
-        webView.evaluateJavaScript(jsCode, completionHandler: nil)
+        
+        // Check if the webView is the activeWebView
+        if webView == activeWebView {
+            // Inject JavaScript for the active web view
+            let jsCodeActive = """
+                // Unmute and play the video
+                document.querySelector('video').muted = false;
+                document.querySelector('video').play();
+            """
+            let jsCode = jsCodeCommon + jsCodeActive
+            webView.evaluateJavaScript(jsCode, completionHandler: nil)
+        } else {
+            // Inject JavaScript for the inactive web view
+            let jsCodeInactive = """
+                // Mute and pause the video
+                document.querySelector('video').muted = true;
+                document.querySelector('video').pause();
+            """
+            let jsCode = jsCodeCommon + jsCodeInactive
+            webView.evaluateJavaScript(jsCode, completionHandler: nil)
+        }
     }
+
     
     // Implement other WKNavigationDelegate methods as needed
 }
