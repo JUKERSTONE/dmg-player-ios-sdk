a7e6540d0f91d6cb30e2ed06df52f8723940cb26
TSB1999 <hakeems@quickcommerce.org>
2024-02-28 06:26:55 +0000

    [iDMG] : startup changes

---
+++ Sources/dmg-player-ios-sdk/DMGPlayerSDK/Player.swift
@@ -0,0 +1,105 @@
+// WKNavigation.swift
+
+import SwiftUI
+import WebKit
+
+// Make sure to conform to WKNavigationDelegate if needed.
+@available(iOS 13.0, *)
+extension DMGPlayerSDK {
+    func loadVideoInPrimaryWebView(url: URL) {
+            let request = URLRequest(url: url)
+            primaryWebView.load(request)
+    }
+    
+    func loadVideoInSecondaryWebView(url: URL) {
+            let request = URLRequest(url: url)
+            secondaryWebView.load(request)
+    }
+
+    func preloadNextWebView() {
+        guard let nextIsrc = queue.first else {
+            print("Queue is empty")
+            return
+        }
+
+        let apiService = APIService.shared
+        let urlString = "https://europe-west1-trx-traklist.cloudfunctions.net/TRX_DEVELOPER/trx/music/\(nextIsrc)"
+        
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        apiService.fetchData(from: url) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    guard let urlStringWithQuotes = String(data: data, encoding: .utf8) else {
+                        print("The data received could not be converted to a string.")
+                        return
+                    }
+                    
+                    print(urlStringWithQuotes)
+                    // Remove quotation marks from the string
+                    let urlString = urlStringWithQuotes.trimmingCharacters(in: CharacterSet(charactersIn: "\""))
+                 
+                    // Validate if the cleaned string is a valid URL
+                    guard let videoURL = URL(string: urlString) else {
+                        print("The cleaned string is not a valid URL: \(urlString)")
+                        return
+                    }
+                    
+                    // Load the URL in the inactive WebView
+                        if self?.isPrimaryActive == true {
+                            self?.loadVideoInSecondaryWebView(url: videoURL)
+                        } else {
+                            self?.loadVideoInPrimaryWebView(url: videoURL)
+                        }
+                    
+                case .failure(let error):
+                    print("Error fetching data: \(error)")
+                }
+            }
+        }
+    }
+
+    
+    private func muteAndPause(webView: WKWebView) {
+        let script = "window.trakStarVideo.muted = true; window.trakStarVideo.pause();"
+        webView.evaluateJavaScript(script, completionHandler: nil)
+    }
+    
+    func play(webView: WKWebView) {
+        let script = """
+            if (window.trakStarVideo) {
+                window.trakStarVideo.muted = false;
+                window.trakStarVideo.play();
+            
+                window.trakStarVideo.requestPictureInPicture().then(() => {
+                    const message = {
+                        eventType: 'enablePiP',
+                        data: 'PiP initiated successfully.'
+                    };
+                    window.webkit.messageHandlers.player.postMessage(JSON.stringify(message));
+                }).catch(error => {
+                    const message = {
+                        eventType: 'enablePiP',
+                        data: 'PiP initiation failed: ' + error.message
+                    };
+                    window.webkit.messageHandlers.player.postMessage(JSON.stringify(message));
+                });
+            } else {
+                const message = {
+                    eventType: 'enablePiP',
+                    data: 'No video element found.'
+                };
+                window.webkit.messageHandlers.player.postMessage(JSON.stringify(message));
+            };
+            """
+        
+        webView.evaluateJavaScript(script, completionHandler: nil)
+    }
+}
+
+
+
---
+++ Sources/dmg-player-ios-sdk/DMGPlayerSDK/WKMessage.swift
@@ -0,0 +1,69 @@
+// DMGPlayerSDK+MessageHandling.swift
+
+import Foundation
+import WebKit
+
+// MARK: - WKScriptMessageHandler
+@available(iOS 13.0, *)
+extension DMGPlayerSDK {
+
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "player" {
+            // Attempt to convert the message body directly into a String
+            guard let messageString = message.body as? String else {
+                print("The message body is not a string.")
+                return
+            }
+
+            // Attempt to convert the string to Data
+            guard let jsonData = messageString.data(using: .utf8) else {
+                print("Could not convert message string to Data.")
+                return
+            }
+
+            // Attempt to deserialize the JSON string into a dictionary
+            guard let messageDict = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] else {
+                print("Failed to deserialize JSON string into a dictionary.")
+                return
+            }
+
+            // Now we can safely check for the eventType
+            guard let eventType = messageDict["eventType"] as? String else {
+                print("The 'eventType' is not a string or not present in the message body.")
+                return
+            }
+
+            // Handle known events
+            switch eventType {
+            case "videoProgress":
+                // Check if the progress data is a Double
+                if let progressData = messageDict["data"] as? Double {
+                    // Process the progress data
+                    if progressData > 80.0 && !self.hasPreloadedNextWebview {
+                        self.preloadNextWebView() // Call your preload function here
+                        self.hasPreloadedNextWebview = true // Set the flag to true after preloading
+                    }
+                } else {
+                    print("The 'data' for 'videoProgress' is not a Double or not present in the message body.")
+                }
+            case "videoEnded":
+                if self.isPrimaryActive == true && self.hasPreloadedNextWebview {
+                    self.play(webView: self.secondaryWebView)
+                    isPrimaryActive = false
+                    primaryWebView.loadHTMLString("<html><html>", baseURL: <#T##URL?#>)
+                    hasPreloadedNextWebview = false
+                } else if self.isPrimaryActive == false && self.hasPreloadedNextWebview {
+                    self.play(webView: self.primaryWebView)
+                    isPrimaryActive = true
+                    secondaryWebView.loadHTMLString("<html><html>", baseURL: <#T##URL?#>)
+                    hasPreloadedNextWebview = false
+                }
+            default:
+                print("Unknown event type received: \(eventType)")
+            }
+        } else {
+            print("Received a message from an unexpected handler: \(message.name)")
+        }
+    }
+    
+}
