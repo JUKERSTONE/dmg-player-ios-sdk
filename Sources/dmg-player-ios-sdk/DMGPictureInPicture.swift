e01a0e26742e0c27fa77ed104ad4a68a34b77564
TSB <tobishittabey@gmail.com>
2024-02-28 19:55:15 +0000

    [iDMG] : views

--- Sources/dmg-player-ios-sdk/DMGPictureInPicture.swift
+++ Sources/dmg-player-ios-sdk/DMGPictureInPicture.swift
@@ -21,9 +21,9 @@ public struct DMGPictureLicense: UIViewRepresentable {
         primaryWebView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             primaryWebView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            primaryWebView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
+            primaryWebView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            primaryWebView.heightAnchor.constraint(equalToConstant: 0) // Set height to zero
         ])
-        primaryWebView.alpha = 0.1 // Make the primaryWebView invisible
 
         // Secondary WebView setup
         let secondaryWebView = sdk.secondaryWebView
@@ -32,16 +32,14 @@ public struct DMGPictureLicense: UIViewRepresentable {
         secondaryWebView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             secondaryWebView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            secondaryWebView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
+            secondaryWebView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            secondaryWebView.heightAnchor.constraint(equalToConstant: 0) // Set height to zero
         ])
-        secondaryWebView.alpha = 0.1 // Make the secondaryWebView invisible
         
-        // Add other views here if necessary
-        // Ensure that any other views are added after the web views
-        // This will ensure that the web views stay at the back
-
+        // The web views are now centered and have no height, but still exist in the view hierarchy
         return containerView
     }
+
 
     
     public func updateUIView(_ uiView: UIView, context: Context) {
