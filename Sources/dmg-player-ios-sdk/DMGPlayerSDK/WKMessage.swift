d52d98417813707ddc6aa49ead4298433b28eaa7
TSB <tobishittabey@gmail.com>
2024-02-28 14:30:42 +0000

    [iDMG] : startup changes

--- Sources/dmg-player-ios-sdk/DMGPlayerSDK/WKMessage.swift
+++ Sources/dmg-player-ios-sdk/DMGPlayerSDK/WKMessage.swift
@@ -60,7 +60,7 @@ extension DMGPlayerSDK {
                         }
                         
                         //                    primaryWebView.loadHTMLString("<html><html>", baseURL: nil)
-                        //                    self.play(webView: self.secondaryWebView)
+                                            self.play(webView: self.secondaryWebView)
                     } else if self.isPrimaryActive == false {
                         self.isPrimaryActive = true
                         self.hasPreloadedNextWebview = false
@@ -69,6 +69,7 @@ extension DMGPlayerSDK {
                         } else {
                             // If the index is already at the end of the queue, print a message
                             print("Index is at the end of the queue")
+                            self.play(webView: self.primaryWebView)
                         }
                     }
                 } else {
@@ -80,9 +81,7 @@ extension DMGPlayerSDK {
                         } else {
                             print("Index is at the end of the queue")
                         }
-                        
-                        //                    primaryWebView.loadHTMLString("<html><html>", baseURL: nil)
-                        //                    self.play(webView: self.secondaryWebView)
+                        self.play(webView: self.secondaryWebView)
                     } else if self.isPrimaryActive == false {
                         self.isPrimaryActive = true
                         self.hasPreloadedNextWebview = false
@@ -92,6 +91,7 @@ extension DMGPlayerSDK {
                             // If the index is already at the end of the queue, print a message
                             print("Index is at the end of the queue")
                         }
+                        self.play(webView: self.primaryWebView)
                     }
                 }
                 
