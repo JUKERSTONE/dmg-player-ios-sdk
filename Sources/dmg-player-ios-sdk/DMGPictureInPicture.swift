e314c6a1398b1bf79a2a7c6582782be1164a30e6
TSB <tobishittabey@gmail.com>
2024-02-28 19:51:14 +0000

    [iDMG] : views

--- Sources/dmg-player-ios-sdk/DMGPictureInPicture.swift
+++ Sources/dmg-player-ios-sdk/DMGPictureInPicture.swift
@@ -14,30 +14,35 @@ public struct DMGPictureLicense: UIViewRepresentable {
     public func makeUIView(context: Context) -> UIView {
         let containerView = UIView()
         
+        // Primary WebView setup
         let primaryWebView = sdk.primaryWebView
         containerView.addSubview(primaryWebView)
         
         primaryWebView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            primaryWebView.topAnchor.constraint(equalTo: containerView.topAnchor),
-            primaryWebView.widthAnchor.constraint(equalToConstant: 400), // Set width to 200 points
-            primaryWebView.heightAnchor.constraint(equalToConstant: 160), // Maintain aspect ratio
-            primaryWebView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor) // Center horizontally
+            primaryWebView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            primaryWebView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
         ])
+        primaryWebView.alpha = 0 // Make the primaryWebView invisible
 
+        // Secondary WebView setup
         let secondaryWebView = sdk.secondaryWebView
         containerView.addSubview(secondaryWebView)
-
+        
         secondaryWebView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            secondaryWebView.topAnchor.constraint(equalTo: secondaryWebView.bottomAnchor, constant: 8), // Add spacing between the web views
-            secondaryWebView.widthAnchor.constraint(equalToConstant: 400),
-            secondaryWebView.heightAnchor.constraint(equalToConstant: 160), // Match height with active web view
-            secondaryWebView.centerXAnchor.constraint(equalTo: containerView.leadingAnchor) // Center horizontally
+            secondaryWebView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            secondaryWebView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
         ])
+        secondaryWebView.alpha = 0 // Make the secondaryWebView invisible
         
+        // Add other views here if necessary
+        // Ensure that any other views are added after the web views
+        // This will ensure that the web views stay at the back
+
         return containerView
     }
+
     
     public func updateUIView(_ uiView: UIView, context: Context) {
         let queuePublisher = sdk.$queue
