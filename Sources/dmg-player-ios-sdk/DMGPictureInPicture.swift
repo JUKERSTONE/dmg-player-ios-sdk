74e3384cac9f7b5c31a16007ec1e741fd4a21d13
TSB <tobishittabey@gmail.com>
2024-03-02 09:14:15 +0000

    [iDMG] : webview

--- Sources/dmg-player-ios-sdk/DMGPictureInPicture.swift
+++ Sources/dmg-player-ios-sdk/DMGPictureInPicture.swift
@@ -1,34 +1,45 @@
+// DMGPictureInPicture.swift
+
 import SwiftUI
 import WebKit
 
 @available(iOS 13.0, *)
 public struct DMGPictureLicense: UIViewRepresentable {
     @ObservedObject var sdk: DMGPlayerSDK
-
+    
     public init(sdk: DMGPlayerSDK) {
-        self.sdk = sdk
-    }
-
+            self.sdk = sdk
+        }
+    
     public func makeUIView(context: Context) -> UIView {
-        let containerView = UIView(frame: CGRect.zero) // Use CGRect.zero to keep it off-screen
+        let containerView = UIView()
         
-        // Configure primary WebView
         let primaryWebView = sdk.primaryWebView
+        containerView.addSubview(primaryWebView)
+        
         primaryWebView.translatesAutoresizingMaskIntoConstraints = false
-        primaryWebView.isHidden = true // WebView is hidden but active
-        UIApplication.shared.keyWindow?.addSubview(primaryWebView) // Add to keyWindow to ensure it is 'active'
+        NSLayoutConstraint.activate([
+            primaryWebView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            primaryWebView.widthAnchor.constraint(equalToConstant: 1),
+            primaryWebView.heightAnchor.constraint(equalToConstant: 1),
+            primaryWebView.centerXAnchor.constraint(equalTo: containerView.leadingAnchor)
+        ])
 
-        // Configure secondary WebView
         let secondaryWebView = sdk.secondaryWebView
+        containerView.addSubview(secondaryWebView)
+
         secondaryWebView.translatesAutoresizingMaskIntoConstraints = false
-        secondaryWebView.isHidden = true // WebView is hidden but active
-        UIApplication.shared.keyWindow?.addSubview(secondaryWebView) // Add to keyWindow to ensure it is 'active'
+        NSLayoutConstraint.activate([
+            secondaryWebView.topAnchor.constraint(equalTo: secondaryWebView.topAnchor),
+            secondaryWebView.widthAnchor.constraint(equalToConstant: 1),
+            secondaryWebView.heightAnchor.constraint(equalToConstant: 1),
+            secondaryWebView.centerXAnchor.constraint(equalTo: containerView.leadingAnchor)
+        ])
         
         return containerView
     }
-
+    
     public func updateUIView(_ uiView: UIView, context: Context) {
-        // Logic remains the same
         let queuePublisher = sdk.$queue
 
         queuePublisher.sink { [weak sdk] updatedQueue in
@@ -42,4 +53,7 @@ public struct DMGPictureLicense: UIViewRepresentable {
             }
         }
     }
+
+
 }
+
