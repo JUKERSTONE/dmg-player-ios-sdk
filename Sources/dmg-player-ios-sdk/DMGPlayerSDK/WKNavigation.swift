3e08e34922c5b6c5d422e61fd3d91b994eac73cc
TSB <tobishittabey@gmail.com>
2024-03-06 16:19:40 +0000

    [iDMG] : background

--- Sources/dmg-player-ios-sdk/DMGPlayerSDK/WKNavigation.swift
+++ Sources/dmg-player-ios-sdk/DMGPlayerSDK/WKNavigation.swift
@@ -8,6 +8,15 @@ extension DMGPlayerSDK: WKNavigationDelegate {
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         let jsCodeCommon = buildCommonJavaScript()
         
+        guard UIApplication.shared.applicationState == .active else {
+            // If the app is in the background, load the web view in an inactive state
+            if webView == bkPrimaryWebView || webView == bkSecondaryWebView {
+                let jsCodeInactive = buildCommonJavaScript() + buildInactiveJavaScript()
+                webView.evaluateJavaScript(jsCodeInactive, completionHandler: nil)
+            }
+            return // Return early if app is in background
+        }
+        
         if self.isPrimaryActive && webView == primaryWebView {
             webView.evaluateJavaScript(buildCommonJavaScript() + buildActiveJavaScript(), completionHandler: nil)
         } else if self.isPrimaryActive && webView == secondaryWebView {
