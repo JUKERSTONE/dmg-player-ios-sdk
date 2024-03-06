77f3d5ac6ad5f60712f33503d50843835cbb23ea
TSB <tobishittabey@gmail.com>
2024-03-06 14:41:42 +0000

    [iDMG] : background

--- Sources/dmg-player-ios-sdk/DMGPlayerSDK/WKNavigation.swift
+++ Sources/dmg-player-ios-sdk/DMGPlayerSDK/WKNavigation.swift
@@ -7,21 +7,21 @@ import WebKit
 extension DMGPlayerSDK: WKNavigationDelegate {
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         let jsCodeCommon = buildCommonJavaScript()
+        let appIsInForeground = UIApplication.shared.applicationState == .active
     
-        if webView == bkPrimaryWebView || webView == bkSecondaryWebView {
+        if appIsInForeground && webView == bkPrimaryWebView || webView == bkSecondaryWebView {
             webView.evaluateJavaScript(jsCodeCommon + buildInactiveJavaScript(), completionHandler: nil)
+        } else {
+            if self.isPrimaryActive && webView == primaryWebView {
+                primaryWebView.evaluateJavaScript(buildCommonJavaScript() + buildActiveJavaScript(), completionHandler: nil)
+            } else if self.isPrimaryActive && webView == secondaryWebView {
+                secondaryWebView.evaluateJavaScript(buildCommonJavaScript() + buildInactiveJavaScript(), completionHandler: nil)
+            } else if !self.isPrimaryActive && webView == primaryWebView {
+                primaryWebView.evaluateJavaScript(buildCommonJavaScript() + buildInactiveJavaScript(), completionHandler: nil)
+            } else if !self.isPrimaryActive && webView == secondaryWebView {
+                secondaryWebView.evaluateJavaScript(buildCommonJavaScript() + buildActiveJavaScript(), completionHandler: nil)
+            }
         }
-        
-        if self.isPrimaryActive && webView == primaryWebView {
-            primaryWebView.evaluateJavaScript(buildCommonJavaScript() + buildActiveJavaScript(), completionHandler: nil)
-        } else if self.isPrimaryActive && webView == secondaryWebView {
-            secondaryWebView.evaluateJavaScript(buildCommonJavaScript() + buildInactiveJavaScript(), completionHandler: nil)
-        } else if !self.isPrimaryActive && webView == primaryWebView {
-            primaryWebView.evaluateJavaScript(buildCommonJavaScript() + buildInactiveJavaScript(), completionHandler: nil)
-        } else if !self.isPrimaryActive && webView == secondaryWebView {
-            secondaryWebView.evaluateJavaScript(buildCommonJavaScript() + buildActiveJavaScript(), completionHandler: nil)
-        }
-        
     }
     
     
